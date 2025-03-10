5ae1878f27faf45957d48c022023b5d8b1a71f2b
daouddd777 <149582644+daouddd777@users.noreply.github.com>
2025-03-10 10:34:01 +0300

    create api service

--- lib/Features/home/domain/entities/book_entity.dart
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -1,14 +1,16 @@
 class BookEntity {
+  final String bookId;
+  final String? image;
   final String title;
-  final String authorName;
-  final num price;
-  final num rating;
-  final String image;
+  final String? authorName;
+  final num? price;
+  final num? rating;
 
   BookEntity(
-      {required this.title,
+      {required this.image,
+      required this.title,
       required this.authorName,
       required this.price,
       required this.rating,
-      required this.image});
+      required this.bookId});
 }
